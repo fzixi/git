8f4279eae86d75563776e688084783379a93eb02
Zixi Feng <you@example.comzixifeng666888@gmail.com>
2021-12-08 16:44:34 -0600

    add problem1

---
+++ Problem1.java
@@ -0,0 +1,8 @@
+public class Problem1 {
+    public static void main(String[] args) {
+        int x=1;
+        while (x<= 694) {
+        System.out.println(x);
+        x *= 2; 
+        }}}
+    
